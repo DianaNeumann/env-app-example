15c54c915375b12a3adb1c2beb21643de2d35609
DmitryKnyazkin <kniazkin_d@goldapple.ru>
2025-03-24 01:00:39 +0300

    feat: add proj files

---
+++ EnvApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var name = Environment.GetEnvironmentVariable("MAJUGA") ?? "foobar";
+        Console.WriteLine($"env: {name}!");
+    }
+}
